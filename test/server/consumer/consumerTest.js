879cde4d2b7d9848a429ec0edc3b677cb6fe7a5b
gutobortolozzo <gutobortolozzo@gmail.com>
2016-02-15 13:15:08 -0200

    Destroy client (SOCKET) when executor drop connection

--- test/server/consumer/consumerTest.js
+++ test/server/consumer/consumerTest.js
@@ -5,7 +5,7 @@ const consumer = require(process.cwd()+"/lib/server/consumer/consumer");
 
 describe("Consumer", () => {
 
-    it("Reject promise when client give error on connect", () => {
+    it("Reject promise when client give error on connection", () => {
 
         return consumer.execute({}, '127.0.0.1', 68001)
             .catch((error) => {
