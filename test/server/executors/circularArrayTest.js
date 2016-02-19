e10752f1cb3e231ddeda97ebaaab1d4648452923
gutobortolozzo <gutobortolozzo@gmail.com>
2016-02-18 22:00:32 -0200

    Circular array to never dereference executors array

--- test/server/executors/circularArrayTest.js
+++ test/server/executors/circularArrayTest.js
@@ -1,4 +1,4 @@
-require('should');
+const should = require('should');
 const CircularArray = require(process.cwd()+"/lib/server/executors/circularArray");
 
 describe("Circular array", () => {
@@ -7,13 +7,11 @@ describe("Circular array", () => {
 
     it("Add two elements and rotate array", () => {
 
-        array.putExecutorIfAbsent({
+        array.putExecutors([{
             host : '127.0.0.2'
-        });
-
-        array.putExecutorIfAbsent({
+        },{
             host : '127.0.0.1'
-        });
+        }]);
 
         const endInOne = array.next();
         const endInTwo = array.next();
@@ -26,19 +24,69 @@ describe("Circular array", () => {
 
     it("Add repeated element and rotate array", () => {
 
-        array.putExecutorIfAbsent({
+        array.putExecutors([{
             host : '127.0.0.1'
-        });
-        
-        array.putExecutorIfAbsent({
+        },{
             host : '127.0.0.1'
-        });
+        }]);
 
         const endInOne = array.next();
         const endInOneAgain = array.next();
 
         endInOne.host.should.be.eql("127.0.0.1");
         endInOneAgain.host.should.be.eql("127.0.0.1");
+        array.size().should.be.eql(1);
+    });
+
+    it("Add intersect element and rotate array", () => {
+
+        array.putExecutors([{
+            host : '127.0.0.1'
+        },{
+            host : '127.0.0.2'
+        }]);
+
+        array.putExecutors([{
+            host : '127.0.0.3'
+        },{
+            host : '127.0.0.2'
+        }]);
+
+        const endInOne = array.next();
+        const endInOneAgain = array.next();
+
+        endInOne.host.should.be.eql("127.0.0.3");
+        endInOneAgain.host.should.be.eql("127.0.0.2");
+        array.size().should.be.eql(2);
+    });
+
+    it("Add executor -> empty executors -> add executor", () => {
+
+        array.putExecutors([{
+            host : '127.0.0.1'
+        }]);
+
+        array.putExecutors([]);
+
+        array.size().should.be.eql(0);
+
+        array.putExecutors([{
+            host : '127.0.0.2'
+        }]);
+
+        const endInOne = array.next();
+
+        endInOne.host.should.be.eql("127.0.0.2");
+        array.size().should.be.eql(1);
+    });
+
+    it("Empty executors rotation should not produce undefined executor", () => {
+
+        array.putExecutors([]);
+        array.size().should.be.eql(0);
+
+        should.not.exist(array.next());
+        array.size().should.be.eql(0);
     });
 
     beforeEach(() => array = new CircularArray);
