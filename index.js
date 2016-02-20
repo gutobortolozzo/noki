3a24302f7ba82c4a1dac54fb757e8d3b2f5e71cf
gutobortolozzo <gutobortolozzo@gmail.com>
2016-02-20 02:10:36 -0200

    fix: Fixing base lib require of client and server index

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const client = require(process.cwd()+'/lib/client/executor/executor');
-const Server = require(process.cwd()+'/lib/server/bridge/bridge');
+const client = require('./lib/client/executor/executor');
+const Server = require('./lib/server/bridge/bridge');
 
 module.exports.client = client;
 module.exports.Server = Server;
