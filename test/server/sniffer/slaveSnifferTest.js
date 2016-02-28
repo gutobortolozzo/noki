650cc10c9ba0d4dfbe12882331bf18f75afae0f2
gutobortolozzo <gutobortolozzo@gmail.com>
2016-02-28 18:12:59 -0300

    fix(Configuration): Optional parameters to configure broadcast addresss and datagram (dgram) port

--- test/server/sniffer/slaveSnifferTest.js
+++ test/server/sniffer/slaveSnifferTest.js
@@ -6,9 +6,14 @@ const sniffer  = require(process.cwd()+"/lib/server/sniffer/slaveSniffer");
 
 describe("Sniffer", () => {
 
+    let selfRegister;
+
     it("Find socket running on port 9902", (done) => {
 
-        const emitter = sniffer.sniff(9902);
+        const emitter = sniffer.sniff({
+            port      : 9902,
+            dgramPort : 41234
+        });
 
         emitter.on("message", (executor) => {
             ip.isV4Format(executor.host).should.be.ok();
@@ -17,11 +22,31 @@ describe("Sniffer", () => {
         });
     });
 
-    let selfRegister;
+    it("Find socket running on port 9905 and dgram port 30001", (done) => {
+
+        selfRegister = register.startExecutorBroadcast({
+            broadcast : "255.255.255.255",
+            dgramPort : 30001
+        });
+
+        const emitter = sniffer.sniff({
+            port      : 9905,
+            dgramPort : 30001
+        });
+
+        emitter.on("message", (executor) => {
+            ip.isV4Format(executor.host).should.be.ok();
+            executor.port.should.be.eql(9905);
+            done();
+        });
+    });
 
     beforeEach(() => {
         sniffer.kill();
-        selfRegister = register.startExecutorBroadcast()
+        selfRegister = register.startExecutorBroadcast({
+            broadcast : "255.255.255.255",
+            dgramPort : 41234
+        });
     });
 
     afterEach(() => selfRegister.close());
